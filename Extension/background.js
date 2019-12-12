bfb185e838beb2e23108665ed04c8d40bf371563
satvik.dandale@gmail.com <satvik.dandale@gmail.com>
2019-12-13 00:46:59 +0530

    Block websites by time limit in extension done.

--- Extension/background.js
+++ Extension/background.js
@@ -5,30 +5,37 @@
     #####################
 */
 
-var list = []   // Blocklist
+var restrictedList = []   // Blocklist
 var blockListFlag = false;  // It will be true when the block list is updated.
+var timeExceededWebsites = []   // List of websites where time limit has been exceeded.
 function setBlockListFlag(input){
     blockListFlag = input;
 }
 
+// Block all the websites if not logged in.
 function blockRequest(details) {
     if (details.url.includes("googleapis"))
         return {cancel: false}
     return {cancel: true};
 }
+// Block specific websites if the websites are restricted.
 function blockSpecific(details){
     for(website in categories["Restricted"]){
-        if (!list.includes(website))
-            list.push(website)
+        if (!restrictedList.includes(website))
+            restrictedList.push(website)
     }
 
-    for(var i=0; i <= list.length; i++){
-        if (details.url.includes(list[i]))
+    for(var i=0; i <= restrictedList.length; i++){
+        if (details.url.includes(restrictedList[i]))
             return {cancel: true};  // Only block specific websites
         }
+    for (var i=0; i <= timeExceededWebsites.length; i++){
+        if (details.url.includes(timeExceededWebsites[i]))
+            return {cancel: true};
+    }
     return {cancel: false};
 }
-
+// Filter for above types of blocking.
 function updateFilters(status) {
     if (status === false){
         if(chrome.webRequest.onBeforeRequest.hasListener(blockRequest))
@@ -41,9 +48,7 @@ function updateFilters(status) {
         chrome.webRequest.onBeforeRequest.addListener(blockSpecific, {urls: ["*://*/*"]}, ["blocking"]);
     }
 }
-
-updateFilters(false);
-
+updateFilters(false);   // By default, block all websites. (NOT LOGGED IN).
 
 //////////////////////////////////////////////////////////
 
@@ -51,12 +56,12 @@ updateFilters(false);
     This script is for the calculation in local domain
 */
 
-
 var tabs_data = {}
 // Use ID of each tab as the key
 // The element contain the running time of each id
 var interval = 1000 // The interval
 categories = {}
+allWebsitesTimeLimt = {}    // Compiled data of all websites and their permitted time limit
 
 /*
     Flags for login Status
@@ -88,14 +93,61 @@ function update_info(){
         chrome.tabs.query({}, function(tabs){
             var current_tabs = [];
             // This list is used to check if multiple tabs of same domain are open
-            for (var i=0; i < tabs.length; i++){
+            for (var i = 0; i < tabs.length; i++){
+                // Get the domain name of the URL on this tab
                 domain = getDomain(tabs[i].url);
                 if (tabs_data[domain] === undefined ){
                     tabs_data[domain] = 0; // The domain name of this tab not found in the list.
                     // That means the tab just opened in the current session.
                 }
-                else if (!(current_tabs.includes(domain)) && !list.includes(domain)){   // Don't update time for blocked tabs
-                    tabs_data[domain] += interval/1000; // Add the interval time in sec not msec
+                else if (!(current_tabs.includes(domain)) && !restrictedList.includes(domain)){   // Don't update time for blocked tabs
+                    // Check the time limit of this domain name
+                    for(var website in allWebsitesTimeLimt){
+                        if (website.includes(domain)){
+                            // Check the time limit
+                            var timeLimit = allWebsitesTimeLimt[domain];
+                            if (tabs_data[domain] >= timeLimit){
+                                // The time limit has been exceeded
+                                // Add the page to the timeLimitReach list
+                                console.log(timeExceededWebsites);
+                                var breakFlag = false;
+                                for(var j = 0; j < timeExceededWebsites.length; j++){
+                                    if (timeExceededWebsites[j].includes(domain)){
+                                        breakFlag = true;
+                                        break;  // The timeLimitReached list already has this domain. Do not add this domain to the list.
+                                    }
+                                }
+                                if (breakFlag === true){
+                                    break   // Next Tab
+                                }
+                                else{
+                                    // Add this domain to the list timeLimitReached
+                                    timeExceededWebsites.push(domain);
+                                }
+                                // Refresh the tab
+                                var refreshCode = "window.location.replace(\"blocked.html\")"
+                                chrome.tabs.executeScript(tabs[i].id, {code: refreshCode});
+                                console.log("Time limit exceeded for " + domain);
+                            }
+                        }
+                    }
+                    var isLimitExceeded = false;
+                    for(var i=0; i < timeExceededWebsites.length; i++){
+                        try{
+                            if (timeExceededWebsites[i].includes(domain)){   // Then don't add the time usage
+                            isLimitExceeded = true;
+                            break;
+                            }
+                        }
+                        catch(e){
+                            console.log(e);
+                        }
+                        
+                    }
+                    if (!isLimitExceeded){
+                        tabs_data[domain] += interval/1000; // Add the interval time in sec not msec
+                        console.log("Time incremented for website: " + domain);
+                    }
                 }
                 current_tabs.push(domain);
             }
@@ -140,6 +192,17 @@ chrome.extension.onConnect.addListener(function(port){
             setUser(user);
             retrieveCategories().then(function(webCategories){
                 categories = webCategories;
+                /*
+                    NOW iterate in this categories dictionary
+                    When a website is found, add that to the allWebsitesTimeLimit dictionary
+                */
+                for(var category in categories){
+                    // For each category: there are websites
+                    category = categories[category];
+                    for(var eachWebsite in category){
+                        allWebsitesTimeLimt[eachWebsite] = category[eachWebsite];
+                    }
+                }
                 updateFilters(true);
             });
         }
